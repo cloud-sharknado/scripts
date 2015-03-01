6f574ef012307d54f06cb01446616d0f01ebf7e6
dangube4ever <dangube4ever@users.noreply.github.com>
2015-03-01 22:40:23 +0100

    Create process_substitution.sh
    Process Substitution

---
+++ bash_hacks/process_substitution.sh
@@ -0,0 +1,7 @@
+# Process Substitution
+#
+# compare the contents of two directory
+diff <(ls $first_directory) <(ls $second_directory)
+
+# Reference
+# http://wiki.bash-hackers.org/syntax/expansion/proc_subst
